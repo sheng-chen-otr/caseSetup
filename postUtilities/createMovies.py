96aedf45d8723573c332cc2250ab22e71e611649
zchen147 <zhi.chen1@ontariotechu.net>
2026-02-15 00:20:34 -0500

    createmovies

--- postUtilities/createMovies.py
+++ postUtilities/createMovies.py
@@ -27,6 +27,8 @@ def generateMovies():
 
         for prefix in prefixes:
             print('\t%s' % (prefix))
+            slices = glob.glob(os.path.join(dir,'%s*.png' % (prefix)))
+            print('\t\t',slices)
             # os.system("ffmpeg -y -framerate 10 -pattern_type glob -i '%s*.png' -vf scale=1920:1080 -c:v libx264 -pix_fmt yuv420p %s.mp4 >> log.createMovies" % (os.path.join(dir),
             #                                                                                                             os.path.join(dir,prefix)))
             
